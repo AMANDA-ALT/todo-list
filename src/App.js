acd8fccd09972089e7dc16fe70709c2782c58e8a
Amanda <amandabaratadev@gmail.com>
2023-06-23 17:45:39 -0300

    Added states needed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
 
+import { useState, useEffect } from 'react'
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from 'react-icons/bs'
+
+const API = "http://localhost:5000";
+
 function App() {
+
+  const [title, setTitle] =  useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+
   return (
     <div className="App">
       <h1>To do list</h1>
